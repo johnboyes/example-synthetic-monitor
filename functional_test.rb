f78bce8f066bdcbef68a1fbc61863070155c3254
John Boyes <john@edinburghwebservices.co.uk>
2015-08-13 15:34:42 +0100

    extracted hash comparison logic

--- functional_test.rb
+++ functional_test.rb
@@ -13,13 +13,17 @@ require 'hashdiff'
   	  @mirage = Mirage::Client.new
   	end
 
+  	def assert_hashes_equal(expected, actual)
+      assert HashDiff.diff(expected, actual) == []
+  	end
+
   	def test_monitoring
   	  @mirage.put('slack', 'Notification received on Slack') { http_method 'POST' }
   	  foreman_thread = Thread.new {`foreman start`}
   	  sleep 10
       foreman_thread.exit
   	  expected_response_body = {"attachments"=> [{"fallback"=>"ALERT: 1 test failed", "color"=>"danger", "title"=>"ALERT: 1 test failed", "fields"=> [{"title"=>"example example of a test which will fail, triggering a notification on Slack", "value"=>"\n\nexpected: 500\n     got: 200\n\n(compared using ==)\n\n# ./spec/example_spec.rb:19\n==================================================="}]}]}
-	  assert HashDiff.diff(expected_response_body, JSON.parse(@mirage.requests(1).body)) == []
+	  assert_hashes_equal expected_response_body, JSON.parse(@mirage.requests(1).body)
     end
 
     def teardown
