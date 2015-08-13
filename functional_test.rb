fb41a81d2cb02784b6e43ae430003989c6be97c1
John Boyes <john@edinburghwebservices.co.uk>
2015-08-13 13:19:35 +0100

    extracted setup and teardown

--- functional_test.rb
+++ functional_test.rb
@@ -8,15 +8,21 @@ require 'hashdiff'
  
   class FunctionalTest < Minitest::Test
 
+  	def setup
+      Mirage.start
+  	  @mirage = Mirage::Client.new
+  	end
+
   	def test_monitoring
-  	  Mirage.start
-  	  mirage = Mirage::Client.new
-  	  mirage.put('slack', 'Notification received on Slack') { http_method 'POST' }
+  	  @mirage.put('slack', 'Notification received on Slack') { http_method 'POST' }
   	  foreman_thread = Thread.new {`foreman start`}
   	  sleep 10
       foreman_thread.exit
   	  expected_response_body = {"attachments"=> [{"fallback"=>"ALERT: 1 test failed", "color"=>"danger", "title"=>"ALERT: 1 test failed", "fields"=> [{"title"=>"example example of a test which will fail, triggering a notification on Slack", "value"=>"\n\nexpected: 500\n     got: 200\n\n(compared using ==)\n\n# ./spec/example_spec.rb:19\n==================================================="}]}]}
-	  assert HashDiff.diff(expected_response_body, JSON.parse(mirage.requests(1).body)) == []
+	  assert HashDiff.diff(expected_response_body, JSON.parse(@mirage.requests(1).body)) == []
+    end
+
+    def teardown
       Mirage.stop
     end
  
