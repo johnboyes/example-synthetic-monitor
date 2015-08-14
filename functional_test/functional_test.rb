a950ec7f26cc218e312b7ec8a0c82f99595cfe9a
John Boyes <john@edinburghwebservices.co.uk>
2015-08-14 12:17:32 +0100

    extracted explaining method

--- functional_test/functional_test.rb
+++ functional_test/functional_test.rb
@@ -15,21 +15,21 @@ require 'hashdiff'
   	  @expected_request_body_sent_to_slack = {"attachments"=> [{"fallback"=>"ALERT: 1 test failed", "color"=>"danger", "title"=>"ALERT: 1 test failed", "fields"=> [{"title"=>"example example of a test which will fail, triggering a notification on Slack", "value"=>"\n\nexpected: 500\n     got: 200\n\n(compared using ==)\n\n# ./spec/example_spec.rb:19\n==================================================="}]}]}
   	end
 
+    def retryable &block
+      yield rescue retry
+    end
+
   	def assert_hashes_equal(expected, actual)
       assert_equal [], HashDiff.diff(expected, actual)
   	end
 
-    def retryable &block
-      begin
-        yield
-      rescue
-        retry
-      end
+    def assert_notification_sent_to_slack
+      assert_hashes_equal @expected_request_body_sent_to_slack, JSON.parse(@mirage.requests(1).body)
     end
 
   	def test_one_test_fails_and_notifies_on_slack
   	  @foreman_thread = Thread.new {`foreman start`}
-  	  retryable { assert_hashes_equal @expected_request_body_sent_to_slack, JSON.parse(@mirage.requests(1).body) }
+  	  retryable { assert_notification_sent_to_slack }
     end
 
     def teardown
