8b65c4dce21cef7db786965fa7f53dedc5d9cc81
John Boyes <john@edinburghwebservices.co.uk>
2015-08-14 12:22:41 +0100

    rename

--- functional_test/functional_test.rb
+++ functional_test/functional_test.rb
@@ -12,7 +12,7 @@ require 'hashdiff'
       Mirage.start
   	  @mirage = Mirage::Client.new
   	  @mirage.put('slack', 'Notification received on Slack') { http_method 'POST' }
-  	  @expected_request_body_sent_to_slack = {"attachments"=> [{"fallback"=>"ALERT: 1 test failed", "color"=>"danger", "title"=>"ALERT: 1 test failed", "fields"=> [{"title"=>"example example of a test which will fail, triggering a notification on Slack", "value"=>"\n\nexpected: 500\n     got: 200\n\n(compared using ==)\n\n# ./spec/example_spec.rb:19\n==================================================="}]}]}
+  	  @expected_slack_notification_request_body = {"attachments"=> [{"fallback"=>"ALERT: 1 test failed", "color"=>"danger", "title"=>"ALERT: 1 test failed", "fields"=> [{"title"=>"example example of a test which will fail, triggering a notification on Slack", "value"=>"\n\nexpected: 500\n     got: 200\n\n(compared using ==)\n\n# ./spec/example_spec.rb:19\n==================================================="}]}]}
   	end
 
     def retryable &block
@@ -24,7 +24,7 @@ require 'hashdiff'
   	end
 
     def assert_notification_sent_to_slack
-      assert_hashes_equal @expected_request_body_sent_to_slack, JSON.parse(@mirage.requests(1).body)
+      assert_hashes_equal @expected_slack_notification_request_body, JSON.parse(@mirage.requests(1).body)
     end
 
   	def test_one_test_fails_and_notifies_on_slack
