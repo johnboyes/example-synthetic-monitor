5ddca06810d40b1392170502eff1b98f463be679
John Boyes <john@edinburghwebservices.co.uk>
2017-03-06 07:34:45 +0000

    Add test coverage for when monitoring tests all pass (#3)
    * Add success scenario test

--- functional_test/functional_test.rb
+++ functional_test/functional_test.rb
@@ -4,34 +4,37 @@ class FunctionalTest < Minitest::Test
   include Foreman::Test
 
   def setup
+    ENV.store 'SLACK_WEBHOOK', 'http://localhost:7001/responses/slack'
     Mirage.start
     @mirage = Mirage::Client.new
     @mirage.put('slack', 'Notification received on Slack') { http_method 'POST' }
-    @expected_slack_notification_request_body = {"attachments"=> [{"fallback"=>"ALERT: 1 test failed", "color"=>"danger", "title"=>"ALERT: 1 test failed", "fields"=> [{"title"=>"example example of a test which will fail, triggering a notification on Slack", "value"=>"\n\nexpected: 500\n     got: 200\n\n(compared using ==)\n\n# ./spec/example_spec.rb:19\n==================================================="}]}]}
+    @mirage.put('success', 'Success notification received') { http_method 'POST' }
+    @expected_slack_notification_request_body = {"attachments"=> [{"fallback"=>"ALERT: 1 test failed", "color"=>"danger", "title"=>"ALERT: 1 test failed", "fields"=> [{"title"=>"failing test example example of a test which will fail, triggering a notification on Slack", "value"=>"\n\nexpected: 500\n     got: 200\n\n(compared using ==)\n\n# ./spec/failure_spec.rb:13\n==================================================="}]}]}
   end
 
-  def test_one_test_fails_and_notifies_on_slack
-    Thread.new {`foreman start`}
-    retryable { assert_notification_sent_to_slack }
+  def test_run_all_specs
+    @monitor_thread = Thread.new { SyntheticMonitor.new.monitor ENV['SLACK_WEBHOOK'] }
+    assert_notification_sent_to_slack
+    assert_no_success_notification_sent
+  end
+
+  def test_run_failure_spec
+    @monitor_thread = Thread.new { SyntheticMonitor.new.monitor 'spec/failure_spec.rb', ENV['SLACK_WEBHOOK'] }
+    assert_notification_sent_to_slack
+    assert_no_success_notification_sent
+  end
+
+  def test_run_success_spec
+    @monitor_thread = Thread.new {
+      monitor = SyntheticMonitor.new(success_notifications_url: success_notifications_post_url)
+      monitor.monitor 'spec/success_spec.rb', ENV['SLACK_WEBHOOK']
+    }
+    assert_success_notification_sent
+    assert_no_notification_sent_to_slack
   end
 
   def teardown
     @mirage.clear
     Mirage.stop
   end
-
-  private
-
-  def retryable &block
-    yield rescue retry
-  end
-
-  def assert_hashes_equal(expected, actual)
-    assert_equal [], HashDiff.diff(expected, actual)
-  end
-
-  def assert_notification_sent_to_slack
-    assert_hashes_equal @expected_slack_notification_request_body, JSON.parse(@mirage.requests(1).body)
-  end
-
 end
--- functional_test/test_helper.rb
+++ functional_test/test_helper.rb
@@ -5,3 +5,50 @@ require 'mirage/client'
 require 'json'
 require 'hashdiff'
 require 'foreman/test'
+
+def retryable &block
+  tries = 0
+  begin
+    yield
+  rescue
+    tries += 1
+    sleep 1
+    retry if tries < 10
+  end
+end
+
+def assert_hashes_equal expected, actual
+  assert_equal [], HashDiff.diff(expected, actual)
+end
+
+def assert_notification_sent_to_slack
+  assert_hashes_equal @expected_slack_notification_request_body, request_body_sent_to_slack
+end
+
+def assert_no_notification_sent_to_slack
+  assert_nil request_body_sent_to_slack
+end
+
+def request_body_sent_to_slack
+  request_body_for_request_id 1
+end
+
+def assert_success_notification_sent
+  assert_equal("all monitoring tests passed", success_request_body_sent["data"])
+end
+
+def success_notifications_post_url
+  "http://localhost:7001/responses/success"
+end
+
+def assert_no_success_notification_sent
+  assert_nil success_request_body_sent
+end
+
+def success_request_body_sent
+  request_body_for_request_id 2
+end
+
+def request_body_for_request_id(request_id)
+  retryable { JSON.parse(@mirage.requests(request_id).body) }
+end
